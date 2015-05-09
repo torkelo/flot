2e09e8316e75ffe35a6c421dfcf6ad412b9d499d
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-05-09 19:29:39 +0200

    Added text size cache to minimize calls to dom size recalculations / re-layouts, significant speedup of drawing axis labels

--- jquery.flot.js
+++ jquery.flot.js
@@ -122,6 +122,7 @@ Licensed under the MIT license.
 		// re-calculating them when the plot is re-rendered in a loop.
 
 		this._textCache = {};
+		this._textSizeCache = window.flotTextSizeCache = window.flotTextSizeCache || {};
 	}
 
 	// Resizes the canvas to the given dimensions.
@@ -368,13 +369,17 @@ Licensed under the MIT license.
 				element.addClass(font);
 			}
 
-			info = styleCache[text] = {
-				width: element.outerWidth(true),
-				height: element.outerHeight(true),
-				element: element,
-				positions: []
-			};
+      info = styleCache[text] = { element: element, positions: [] };
 
+      var size = this._textSizeCache[text];
+			if (size) {
+        info.width = size.width;
+        info.height = size.height;
+			} else {
+        info.width = element.outerWidth(true);
+        info.height = element.outerHeight(true);
+        this._textSizeCache[text] = { width: info.width, height: info.height };
+			}
 			element.detach();
 		}
 
@@ -2010,7 +2015,7 @@ Licensed under the MIT license.
                             ctx.lineTo(xrange.to + subPixel, yrange.to);
                         } else {
                             ctx.moveTo(xrange.from, yrange.to + subPixel);
-                            ctx.lineTo(xrange.to, yrange.to + subPixel);                            
+                            ctx.lineTo(xrange.to, yrange.to + subPixel);
                         }
                         ctx.stroke();
                     } else {
@@ -2525,9 +2530,9 @@ Licensed under the MIT license.
                 radius = series.points.radius,
                 symbol = series.points.symbol;
 
-            // If the user sets the line width to 0, we change it to a very 
+            // If the user sets the line width to 0, we change it to a very
             // small value. A line width of 0 seems to force the default of 1.
-            // Doing the conditional here allows the shadow setting to still be 
+            // Doing the conditional here allows the shadow setting to still be
             // optional even with a lineWidth of 0.
 
             if( lw == 0 )
